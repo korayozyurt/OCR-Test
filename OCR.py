5d4e7ed082f2fdd1df7ada308f41c47d296946ad
koray.ozyurt <koray.ozyurt@outlook.com>
2024-04-12 01:26:06 +0300

    ara yedk

--- OCR.py
+++ OCR.py
@@ -2,59 +2,80 @@ from PIL import Image
 import pytesseract
 import re
 import cv2
+from matplotlib import pyplot as plt
 from pytesseract import Output
 import numpy as np
 
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+# edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-#img = Image.open('test.jpeg')
-img_gray_mode = cv2.imread('test.jpg')
+img = cv2.imread('test.jpg')
 
-lab= cv2.cvtColor(img_gray_mode, cv2.COLOR_BGR2LAB)
-l_channel, a, b = cv2.split(lab)
+#Smooth section
+#Medianını alıp sonrada kernel kadar filtre yapmayı deneyelim blurlamak için sonrada gaussian aldık bakalım
+kernel = np.ones((5,5),np.float32)/25
+img = cv2.medianBlur(img,5)
+img = cv2.filter2D(img,-1,kernel)
+img = cv2.GaussianBlur(img,(5,5),0)
+#Blurtlama kısmı tamam
 
-# Applying CLAHE to L-channel
-# feel free to try different values for the limit and grid size:
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-cl = clahe.apply(l_channel)
+#adaptive threshold
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
 
-# merge the CLAHE enhanced L-channel with the a and b channel
-limg = cv2.merge((cl,a,b))
+#erosion section
+erosionKernel = np.ones((5, 5), np.uint8)
+img = cv2.erode(img, kernel, iterations=1)
 
-# Converting image from LAB Color model to BGR color spcae
-enhanced_img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+img = opening(img)
 
-# Stacking the original image with the enhanced image
-result = np.hstack((img_gray_mode, enhanced_img))
-
-ret,thresh1 = cv2.threshold(img_gray_mode,115,255,cv2.THRESH_BINARY)
-
-#cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
-cv2.imshow('output', thresh1)
+cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
+cv2.imshow('output', img)
 cv2.waitKey()
-#cv2.destroyAllWindows()
 
 # closing all open windows
 cv2.destroyAllWindows()
-text = pytesseract.image_to_string(img_gray_mode,config='--psm 6')
-print(text)
-data = []
-t = []
-prices = []
 
-word = r'([A-Z\s]+)'
-rakam = r'(\d+,\d+)'
 
-for s in text.splitlines():
-    s =  re.sub(r'(?<=\d),\s+', ',', s)
-    w = re.findall(word, s)
-    ra = re.findall(rakam, s)
-    #data.append(eslesmeler)
-    if(ra) :
+d = pytesseract.image_to_data(img, output_type=Output.DICT,config='--oem 3 --psm 6')
+n_boxes = len(d['text'])
+for i in range(n_boxes):
+    if int(d['conf'][i]) > 60:
+        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (125, 255, 125), 2)
 
-        print('word is', w, end= '   ')
-        print(ra)
+cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
+cv2.imshow('output', img)
+cv2.waitKey()
+
+# closing all open windows
+cv2.destroyAllWindows()
+
+# text = pytesseract.image_to_string(img,config='--oem 3 --psm 6')
+# print(text)
+# data = []
+# t = []
+# prices = []
+#
+# word = r'([A-Z\s]+)'
+# rakam = r'(\d+,\d+)'
+#
+# for s in text.splitlines():
+#     s =  re.sub(r'(?<=\d),\s+', ',', s)
+#     w = re.findall(word, s)
+#     ra = re.findall(rakam, s)
+#     #data.append(eslesmeler)
+#     if(ra) :
+#
+#         print('word is', w, end= '   ')
+#         print(ra)
 
 #print(data)
 
