0c29da68b0ed22f0a9ab80a5cf84de1d85c39215
koray.ozyurt <koray.ozyurt@outlook.com>
2024-04-12 02:58:59 +0300

    resmi bölmek işe yaradı

--- OCR.py
+++ OCR.py
@@ -65,65 +65,88 @@ def deskew(image):
 def match_template(image, template):
     return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
 
+def extract_text(img):
+    # Smooth section
+    # Medianını alıp sonrada kernel kadar filtre yapmayı deneyelim blurlamak için sonrada gaussian aldık bakalım
+    kernel = np.ones((5, 5), np.float32) / 25
+    img = cv2.blur(img, (5, 5))
+    img = cv2.medianBlur(img, 9)
+    img = cv2.filter2D(img, -1, kernel)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    # Blurtlama kısmı tamam
+
+    # adaptive threshold
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # erosion section
+    erosionKernel = np.ones((5, 5), np.uint8)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    img = opening(img)
+
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
+    cv2.imshow('output', img)
+    cv2.waitKey()
+
+    # closing all open windows
+    cv2.destroyAllWindows()
+
+    # d = pytesseract.image_to_data(img, output_type=Output.DICT,config='--oem 3 --psm 6')
+    # n_boxes = len(d['text'])
+    # for i in range(n_boxes):
+    #     if int(d['conf'][i]) > 10:
+    #         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+    #         img = cv2.rectangle(img, (x, y), (x + w, y + h), (125, 255, 125), 2)
+    #
+    # cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
+    # cv2.imshow('output', img)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+    text = pytesseract.image_to_string(img, config='--psm 6')
+    print(text)
+    data = []
+    t = []
+    prices = []
+
+    word = r'([A-Z\s]+)'
+    rakam = r'(\d+,\d+)'
+
+    for s in text.splitlines():
+        s = re.sub(r'(?<=\d),\s+', ',', s)
+        w = re.findall(word, s)
+        ra = re.findall(rakam, s)
+        # data.append(eslesmeler)
+        #if (ra):
+            #print('word is', w, end='   ')
+            #print(ra)
+
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 img = cv2.imread('test.jpg')
 
-#Smooth section
-#Medianını alıp sonrada kernel kadar filtre yapmayı deneyelim blurlamak için sonrada gaussian aldık bakalım
-kernel = np.ones((5,5),np.float32)/25
-img = cv2.blur(img, (5,5))
-img = cv2.medianBlur(img,9)
-img = cv2.filter2D(img,-1,kernel)
-img = cv2.GaussianBlur(img,(5,5),0)
-#Blurtlama kısmı tamam
+# aslında 4 yerine 2 de yetiyor.
+height, width, channels = img.shape
+new_height = height // 4
 
-#adaptive threshold
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+for i in range(4):
+    y1 = i * new_height
+    y2 = (i + 1) * new_height
 
-#erosion section
-erosionKernel = np.ones((5, 5), np.uint8)
-img = cv2.erode(img, kernel, iterations=1)
+    tempIm = img[y1:y2,:]
 
-img = opening(img)
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
+    cv2.imshow('output', tempIm)
+    cv2.waitKey()
+    extract_text(tempIm)
 
-cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
-cv2.imshow('output', img)
-cv2.waitKey()
-
-# closing all open windows
-cv2.destroyAllWindows()
-
-#d = pytesseract.image_to_data(img, output_type=Output.DICT,config='--oem 3 --psm 6')
-# n_boxes = len(d['text'])
-# for i in range(n_boxes):
-#     if int(d['conf'][i]) > 10:
-#         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-#         img = cv2.rectangle(img, (x, y), (x + w, y + h), (125, 255, 125), 2)
+# half_height = height//2
 #
-# cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
-# cv2.imshow('output', img)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-text = pytesseract.image_to_string(img,config='--psm 6')
-print(text)
-data = []
-t = []
-prices = []
-
-word = r'([A-Z\s]+)'
-rakam = r'(\d+,\d+)'
-
-for s in text.splitlines():
-    s =  re.sub(r'(?<=\d),\s+', ',', s)
-    w = re.findall(word, s)
-    ra = re.findall(rakam, s)
-    #data.append(eslesmeler)
-    if(ra) :
-
-        print('word is', w, end= '   ')
-        print(ra)
+# top_section = img[:half_height, :]
+# bottom_section = img[half_height:, :]
+#
+# extract_text(top_section)
+# extract_text(bottom_section)
 
 #print(data)
 
